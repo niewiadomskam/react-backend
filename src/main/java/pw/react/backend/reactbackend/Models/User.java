26929d24867b89cf8bce97d2dd0e561375cded78
niewiadomskam <51495809+niewiadomskam@users.noreply.github.com>
2019-11-01 22:45:45 +0100

    Lab3Ex2 - create User entity

---
+++ src/main/java/pw/react/backend/reactbackend/Models/User.java
@@ -0,0 +1,80 @@
+package pw.react.backend.reactbackend.Models;
+
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+
+
+@Entity
+@Table(name = "Users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+
+    @Column(nullable = false)
+    private String FirstName;
+
+    @Column(nullable = false)
+    private String LastName;
+
+    private Date BirthDate;
+
+    @Column(nullable = false)
+    private String Login;
+
+    @Column(nullable = false)
+    private boolean IsActive;
+
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(long id) {
+        Id = id;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.LastName = lastName;
+    }
+
+    public Date getBirthDate() {
+        return BirthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        BirthDate = birthDate;
+    }
+
+    public String getLogin() {
+        return Login;
+    }
+
+    public void setLogin(String login) {
+        Login = login;
+    }
+
+    public boolean getIsActive() {
+        return IsActive;
+    }
+
+    public void setIsActive(boolean isActive) {
+        IsActive = isActive;
+    }
+}
