6b92a4bdf803f53b97d0572a098dda77189d7b3e
niewiadomskam <51495809+niewiadomskam@users.noreply.github.com>
2019-11-02 00:24:31 +0100

    Lab3Ex2

---
+++ src/main/java/pw/react/backend/reactbackend/Repositories/UsersRepository.java
@@ -0,0 +1,11 @@
+package pw.react.backend.reactbackend.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import  pw.react.backend.reactbackend.Models.User;
+
+import java.util.List;
+
+public interface UsersRepository extends JpaRepository<User, String> {
+    List<User> findByLogin(String login);
+
+}
